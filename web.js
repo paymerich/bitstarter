9dce73fcbd7b689b8761f045042d28644456bedf
Pablo Aymerich <paymerich@gmail.com>
2014-01-29 16:11:51 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+fs.readFileSync('./index.html' );
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
